d34ab0031dee21d46a7bd7c8a529cbc8f8ea78f2
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2022-04-25 17:42:38 +0500

    push

---
+++ POS_System/POS_System.Repositories/Interfaces/IDepartmentInterface.cs
@@ -0,0 +1,13 @@
+﻿using POS_System.Domains.Selling;
+
+namespace POS_System.Repositories.Interfaces
+{
+    public interface IDepartmentInterface
+    {
+        Task<List<Department>> GetDepartmentsAsync();
+        Task<Department> GetDepartmentByIdAsync(Guid id);
+        Task<Department> AddDepartmentAsync(Department department);
+        Task<Department> UpdateDepartmentAsync(Department department);
+        Task DeleteDepartmentAsync(Guid departmentId);
+    }
+}
