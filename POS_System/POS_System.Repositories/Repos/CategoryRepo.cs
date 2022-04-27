22962d3eb1f786759d655d5131b98a87af69d984
davronoff <ilyosbekdavronov009@gmail.com>
2022-04-27 16:11:46 +0500

    commit

---
+++ POS_System/POS_System.Repositories/Repos/CategoryRepo.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS_System.Data;
+using POS_System.Domains.Selling;
+using POS_System.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_System.Repositories.Repos
+{
+    public class CategoryRepo : ICategoryInterface
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryRepo(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<Category> AddCategoryAsync(Category category)
+        {
+            _dbContext.Categories.Add(category);
+            _dbContext.SaveChanges();
+            return Task.FromResult(category);
+        }
+
+        public Task DeleteCategoryAsync(Guid categoryId)
+        {
+            _dbContext.Categories.Remove(_dbContext.Categories.FirstOrDefault(p => p.Id == categoryId));
+            _dbContext.SaveChanges();
+            return Task.FromResult(0);
+        }
+
+        public Task<List<Category>> GetCategoriesAsync() =>
+            _dbContext.Categories.ToListAsync();
+
+        public Task<Category> GetCategoryAsync(Guid categoryId) =>
+            _dbContext.Categories.FirstOrDefaultAsync(p => p.Id == categoryId);
+
+        public Task<Category> UpdateCategoryAsync(Category category)
+        {
+            _dbContext.Categories.Update(category);
+            _dbContext.SaveChanges();
+            return Task.FromResult(category);
+        }
+    }
+}
---
+++ POS_System/POS_System.Repositories/Repos/ClientRepo.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS_System.Data;
+using POS_System.Domains.Selling;
+using POS_System.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_System.Repositories.Repos
+{
+    public class ClientRepo : IClientInterface
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ClientRepo(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<Client> AddClientAsync(Client client)
+        {
+            _dbContext.Clients.Add(client);
+            _dbContext.SaveChanges();
+            return Task.FromResult(client);
+        }
+
+        public Task DeleteClientAsync(Guid clientId)
+        {
+            _dbContext.Clients.Remove(_dbContext.Clients.FirstOrDefault(p => p.Id == clientId));
+            _dbContext.SaveChanges();
+            return Task.FromResult(0);
+        }
+
+        public Task<Client> GetClientAsync(Guid clientId) =>
+            _dbContext.Clients.FirstOrDefaultAsync(p => p.Id == clientId);
+
+        public Task<List<Client>> GetClientsAsync() =>
+            _dbContext.Clients.ToListAsync();
+
+        public Task<Client> UpdateClientAsync(Client client)
+        {
+            _dbContext.Clients.Update(client);
+            _dbContext.SaveChanges();
+            return Task.FromResult(client);
+        }
+    }
+}
