b5ccd3abeff1ae4aaa5a8f8fd16abeec81126123
davronoff <ilyosbekdavronov009@gmail.com>
2022-04-29 16:42:05 +0500

    selling repo and interface end

---
+++ POS_System/POS_System.Repositories/Repos/Selling/SellingProccessRepo.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS_System.Data;
+using POS_System.Domains.Selling;
+using POS_System.Repositories.Interfaces.Selling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_System.Repositories.Repos.Selling
+{
+    public class SellingProccessRepo : ISellingProccessInterface
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public SellingProccessRepo(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<SellingProccess> AddSellingProccessAsync(SellingProccess sellingProccess)
+        {
+            _dbContext.SellingProccesses.Add(sellingProccess);
+            _dbContext.SaveChanges();
+            return Task.FromResult(sellingProccess);
+        }
+
+        public Task DeeleteSellingProccessAsync(Guid sellingproccessId)
+        {
+            _dbContext.SellingProccesses.Remove(_dbContext.SellingProccesses.FirstOrDefault(p => p.Id == sellingproccessId));
+            _dbContext.SaveChanges();
+            return Task.CompletedTask;
+        }
+
+        public Task<List<SellingProccess>> GetAllSellingProccessAsync() =>
+            _dbContext.SellingProccesses.ToListAsync();
+
+        public Task<SellingProccess> GetSellingProccessAsync(Guid sellingproccessId) =>
+            _dbContext.SellingProccesses.FirstOrDefaultAsync(p => p.Id == sellingproccessId);
+
+        public Task<SellingProccess> UpdateSellingProccessAsync(SellingProccess sellingProccess)
+        {
+            _dbContext.SellingProccesses.Update(sellingProccess);
+            _dbContext.SaveChanges();
+            return Task.FromResult(sellingProccess);
+        }
+    }
+}
