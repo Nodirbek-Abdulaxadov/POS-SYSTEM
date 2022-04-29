8549fba086e9d2707dee751b3daf40312c0f9f9c
davronoff <ilyosbekdavronov009@gmail.com>
2022-04-29 15:13:05 +0500

    SOME

---
+++ POS_System/POS_System.Repositories/Interfaces/Selling/IOrderInterface.cs
@@ -0,0 +1,18 @@
+﻿using POS_System.Domains.Selling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_System.Repositories.Interfaces.Selling
+{
+    public interface IOrderInterface
+    {
+        Task<List<Order>> GetOrdersAsync();
+        Task<Order> GetOrderAsync(Guid orderId);
+        Task<Order> AddOrderAsync(Order order);
+        Task<Order> UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(Guid orderId);
+    }
+}
---
+++ POS_System/POS_System.Repositories/Repos/Selling/OrderRepo.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS_System.Data;
+using POS_System.Domains.Selling;
+using POS_System.Repositories.Interfaces.Selling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_System.Repositories.Repos.Selling
+{
+    public class OrderRepo : IOrderInterface
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public OrderRepo(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<Order> AddOrderAsync(Order order)
+        {
+            _dbContext.Orders.Add(order);
+            _dbContext.SaveChanges();
+            return Task.FromResult(order);
+        }
+
+        public Task DeleteOrderAsync(Guid orderId)
+        {
+            _dbContext.Orders.Remove(_dbContext.Orders.FirstOrDefault(p => p.Id == orderId));
+            _dbContext.SaveChanges();
+            return Task.CompletedTask;
+        }
+
+        public Task<Order> GetOrderAsync(Guid orderId) =>
+            _dbContext.Orders.FirstOrDefaultAsync(p => p.Id == orderId);
+
+        public Task<List<Order>> GetOrdersAsync() =>
+            _dbContext.Orders.ToListAsync();
+
+        public Task<Order> UpdateOrderAsync(Order order)
+        {
+            _dbContext.Orders.Update(order);
+            _dbContext.SaveChanges();
+            return Task.FromResult(order);
+        }
+    }
+}
