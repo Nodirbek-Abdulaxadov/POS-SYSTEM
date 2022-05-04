2ec7540a881469e37fd11d3fad714ab5230bd4ce
davronoff <ilyosbekdavronov009@gmail.com>
2022-05-04 17:31:27 +0500

    commit

---
+++ POS_System/POS_System.Repositories/Interfaces/Inventory/ITransactionProccessInterface.cs
@@ -0,0 +1,18 @@
+﻿using POS_System.Domains.Inventory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_System.Repositories.Interfaces.Inventory
+{
+    public interface ITransactionProccessInterface
+    {
+        Task<List<TransactionProccess>> GetAllTransactionProccessAsync();
+        Task<TransactionProccess> GetTransactionProccessAsync(Guid transactionproccessId);
+        Task<TransactionProccess> AddTransactionProccessAsync(TransactionProccess transactionproccess);
+        Task<TransactionProccess> UpdateTransactionProccessAsync(TransactionProccess transactionproccess);
+        Task DeleteTransactionProccessAsync(Guid transactionproccessId);
+    }
+}
