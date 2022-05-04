53d814bf61a4ffbe39355c3dcae8022cbce037c1
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2022-05-04 17:22:57 +0500

    fsdgdfs

--- POS_System/POS_System.Repositories/Interfaces/Inventory/ITransactionInterface.cs
+++ POS_System/POS_System.Repositories/Interfaces/Inventory/ITransactionInterface.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Transactions;
+using POS_System.Domains.Inventory;
 
 namespace POS_System.Repositories.Interfaces.Inventory
 {
