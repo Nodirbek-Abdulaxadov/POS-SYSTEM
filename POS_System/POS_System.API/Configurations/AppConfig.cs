a265d362b92072637e48ee2c5bed9535a26789bd
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2022-05-04 15:29:56 +0500

    category DI

--- POS_System/POS_System.API/Configurations/AppConfig.cs
+++ POS_System/POS_System.API/Configurations/AppConfig.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using POS_System.Data;
 using POS_System.Repositories.Interfaces.Identity;
+using POS_System.Repositories.Interfaces.Selling;
 using POS_System.Repositories.Repos.Identity;
+using POS_System.Repositories.Repos.Selling;
 
 namespace POS_System.API.Configurations
 {
@@ -17,6 +19,7 @@ namespace POS_System.API.Configurations
         {
             services.AddTransient<IAdminInterface, AdminRepo>();
             services.AddTransient<IAdminRoleInterface, AdminRoleRepo>();
+            services.AddTransient<ICategoryInterface, CategoryRepo>();
         }
 
         public static void AddDbContexts(this IServiceCollection services, ConfigurationManager configuration )
