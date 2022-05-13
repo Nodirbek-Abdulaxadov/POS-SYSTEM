efefe8ae9ec92b52303d86e4443fdb1ade161faf
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2022-05-13 16:14:47 +0500

    some also

--- POS_System/POS_System.API/Configurations/AppConfig.cs
+++ POS_System/POS_System.API/Configurations/AppConfig.cs
@@ -2,11 +2,9 @@
 using POS_System.Data;
 using POS_System.Repositories.Interfaces.Identity;
 using POS_System.Repositories.Interfaces.Inventory;
-using POS_System.Repositories.Interfaces.Report;
 using POS_System.Repositories.Interfaces.Selling;
 using POS_System.Repositories.Repos.Identity;
 using POS_System.Repositories.Repos.Inventory;
-using POS_System.Repositories.Repos.Report;
 using POS_System.Repositories.Repos.Selling;
 
 namespace POS_System.API.Configurations
@@ -34,7 +32,6 @@ namespace POS_System.API.Configurations
             services.AddTransient<ITransactionInterface, TransactionRepo>();
             services.AddTransient<ISellingProccessInterface, SellingProccessRepo>();
             services.AddTransient<ITransactionProccessInterface, TransactionProccessRepo>();
-            services.AddTransient<IMainReportInterface, MainReportRepo>();
             
         }
 
