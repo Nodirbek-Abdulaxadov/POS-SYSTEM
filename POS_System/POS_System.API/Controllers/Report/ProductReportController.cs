af8c433a2cafa38d76277d25dbe8baf6366eb761
davronoff <ilyosbekdavronov009@gmail.com>
2022-05-18 16:09:26 +0500

    some

---
+++ POS_System/POS_System.API/Controllers/Report/ProductReportController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POS_System.BL.Interfaces;
+using POS_System.Domains.Selling;
+
+namespace POS_System.API.Controllers.Report
+{
+    [ApiController,Route("api/[controller]")]
+    public class ProductReportController: ControllerBase
+    {
+        private readonly IProductReportInterface reportInterface;
+
+        public ProductReportController(IProductReportInterface reportInterface)
+        {
+            this.reportInterface = reportInterface;
+        }
+        [HttpGet, Route("getallreport")]
+        public async Task<IActionResult> ProductAllReport(string productName)
+        {
+            Product product = new Product();
+            if(productName != product.Name)
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductAllReport(productName));
+        }
+        [HttpGet, Route("todaysreport")]
+        public async Task<IActionResult> TodaysReport(string productName)
+        {
+
+        }
+    }
+}
