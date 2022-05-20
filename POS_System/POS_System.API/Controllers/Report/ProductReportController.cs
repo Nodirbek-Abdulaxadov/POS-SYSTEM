9a603e89657ce6cb860a0a56647e318e78ee7af3
davronoff <ilyosbekdavronov009@gmail.com>
2022-05-20 17:06:21 +0500

    productcontroller

--- POS_System/POS_System.API/Controllers/Report/ProductReportController.cs
+++ POS_System/POS_System.API/Controllers/Report/ProductReportController.cs
@@ -17,18 +17,22 @@ namespace POS_System.API.Controllers.Report
             this.reportInterface = reportInterface;
             this.product = product;
         }
-        [HttpGet, Route("getallreport/{productName}")]
-        public async Task<IActionResult> AllReport(string productName)
+
+        //barcha hisoboti
+        [HttpGet, Route("allreport/{productName}")]
+        public async Task<IActionResult> ProductAllReport(string productName)
         {
             if (!(await ProductExist(productName)))
             {
                 return BadRequest();
             }
             
-            return Ok(await reportInterface.ProductAllReport(productName));
+            return Ok(await reportInterface.ProductTodaysReport(productName));
         }
-        [HttpGet, Route("todaysreport")]
-        public async Task<IActionResult> TodaysReport(string productName)
+
+        //bugunlik hisobot
+        [HttpGet, Route("todaysreport/{productName}")]
+        public async Task<IActionResult> ProductTodaysReport(string productName)
         {
             if (!(await ProductExist(productName)))
             {
@@ -36,6 +40,101 @@ namespace POS_System.API.Controllers.Report
             }
             return Ok(await reportInterface.ProductTodaysReport(productName));
         }
+        //kunlik hisobot
+        [HttpGet,Route("dailyreport/{productName}/{date}")]
+        public async Task<IActionResult> ProductDailyReport(string productName, string date)
+        {
+            if(!(await ProductExist(productName)))
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductDailyReport(productName, date));
+        }
+
+        //oxirgi haftalik hisobot
+        [HttpGet, Route("lastweeklreport/{productName}")]
+        public async Task<IActionResult> ProductLastWeeklyReport(string productName)
+        {
+            if(!(await ProductExist(productName)))
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductLastWeeklyReport(productName));
+        }
+
+        //oxirgi oylik hisobot
+        [HttpGet, Route("lastmonthlreport/{productName}")]
+        public async Task<IActionResult> ProductLastMonthlyReport(string productName)
+        {
+            if(!(await ProductExist(productName)))
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductLastMonthlyReport(productName));
+        }
+
+        //oxirgi yillik hisobot
+        [HttpGet, Route("lastyearlreport/{productName}")]
+        public async Task<IActionResult> ProductLastYearlyReport(string productName)
+        {
+            if (!(await ProductExist(productName)))
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductLastYearlyReport(productName));
+        }
+
+        //berilgan muddat oralig'idagi hisobot
+        [HttpGet, Route("productreport/{productName}/{startDate}/{endDate}")]
+        public async Task<IActionResult> ProductReport(string productName, string startDate, string endDate)
+        {
+            if (!(await ProductExist(productName)))
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductReport(productName, startDate, endDate));
+        }
+        //oxirgi N kunlik hisobot
+        [HttpGet, Route("ndaysreport/{productName}/{N}")]
+        public async Task<IActionResult> ProductLastNDaysReport(string productName, int N)
+        {
+            if (!(await ProductExist(productName)))
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductLastNDaysReport(productName, N));
+        }
+        //oxirgi N haftalif hisobot
+        [HttpGet, Route("nweeksreport/{productName}/{N}")]
+        public async Task<IActionResult> ProductLastNWeeksReport(string productName, int N)
+        {
+            if (!(await ProductExist(productName)))
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductLastNWeeksReport(productName, N));
+        }
+        //oxirgi N oylik hisobot
+        [HttpGet, Route("nmonthsreport/{productName}/{N}")]
+        public async Task<IActionResult> ProductLastNMonthsReport(string productName, int N)
+        {
+            if (!(await ProductExist(productName)))
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductLastNMonthsReport(productName, N));
+        }
+        //oxirgi N yillik hisobot
+        [HttpGet, Route("nyearsreport/{productName}/{N}")]
+        public async Task<IActionResult> ProductLastNYearsReport(string productName, int N)
+        {
+            if (!(await ProductExist(productName)))
+            {
+                return BadRequest();
+            }
+            return Ok(await reportInterface.ProductLastNYearsReport(productName, N));
+        }
+
 
         [NonAction]
         private async Task<bool> ProductExist(string name)
